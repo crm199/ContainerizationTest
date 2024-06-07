2065e4fbf145b3ae517e26af50a5871952e56a5a
Chris Murphy <crm199@georgetown.edu>
2024-06-07 11:35:14 -0400

    test2

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -14,7 +14,7 @@ import os
 
 import dj_database_url
 
-from django.utils.six.moves.urllib.parse import urlparse
+#from django.utils.six.moves.urllib.parse import urlparse
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
