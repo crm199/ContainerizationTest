41416c11a7dd60c545b9abe557f965012184b308
Christopher Murphy <christopher.murphy@dev2330.columbia.indepth.com>
2024-06-07 09:03:40 -0400

    Initial commit

---
+++ locallibrary/catalog/urls.py
@@ -0,0 +1,33 @@
+from django.conf.urls import url
+from . import views
+from django.conf.urls import include
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^books/$', views.BookListView.as_view(), name='books'),
+    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
+    url(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
+]
+
+urlpatterns += [
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+]
+
+urlpatterns += [
+    url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+]
+
+urlpatterns += [
+    url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+urlpatterns += [
+    url(r'^allborrowed/$', views.AllLoanedBooksListView.as_view(), name='all-borrowed'),
+]
+
+urlpatterns += [
+    url(r'^author/create/$', views.AuthorCreate.as_view(), name='author-create'),
+    url(r'^author/(?P<pk>\d+)/update/$', views.AuthorUpdate.as_view(), name='author-update'),
+    url(r'^author/(?P<pk>\d+)/delete/$', views.AuthorDelete.as_view(), name='author-delete'),
+]
