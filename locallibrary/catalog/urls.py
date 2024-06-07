aad482e9bedb6ca2adcf451ec55e6027bc413420
Chris Murphy <crm199@georgetown.edu>
2024-06-07 12:49:31 -0400

    test2

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,33 +1,33 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 from django.conf.urls import include
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^books/$', views.BookListView.as_view(), name='books'),
-    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
-    url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
-    url(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>/', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
 
 urlpatterns += [
-    url(r'^accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 urlpatterns += [
-    url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 ]
 
 urlpatterns += [
-    url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
 ]
 
 urlpatterns += [
-    url(r'^allborrowed/$', views.AllLoanedBooksListView.as_view(), name='all-borrowed'),
+    path('allborrowed/', views.AllLoanedBooksListView.as_view(), name='all-borrowed'),
 ]
 
 urlpatterns += [
-    url(r'^author/create/$', views.AuthorCreate.as_view(), name='author-create'),
-    url(r'^author/(?P<pk>\d+)/update/$', views.AuthorUpdate.as_view(), name='author-update'),
-    url(r'^author/(?P<pk>\d+)/delete/$', views.AuthorDelete.as_view(), name='author-delete'),
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
 ]
--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -13,23 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
-from django.conf.urls import include
+from django.urls import include, path
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
 
 urlpatterns += [
-    url(r'^catalog/', include('catalog.urls')),
+    path('catalog/', include('catalog.urls')),
 ]
 
 urlpatterns += [
-    url(r'^$', RedirectView.as_view(url='/catalog/', permanent=True)),
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
